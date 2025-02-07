fc6db8d98ced58d569bbc9c1bad9aeb02abfc1c2
Som-Kesharwani <developer.kesharwanisom@gmail.com>
2025-02-07 18:32:11 +0000

    Add init method inside the mongoDBCOnnection

--- database/mongoDBConnection.go
+++ database/mongoDBConnection.go
@@ -10,6 +10,12 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var Client *mongo.Client
+
+func init() {
+	Client = DBInstance()
+}
+
 func DBInstance() *mongo.Client {
 	MongoDB := flag.String("mongo", "mongodb://localhost:27017", "MongoDB connection string")
 
@@ -31,8 +37,6 @@ func DBInstance() *mongo.Client {
 	return client
 
 }
-
-var Client *mongo.Client = DBInstance()
 
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
 	var collection *mongo.Collection = Client.Database("Test").Collection(collectionName)
